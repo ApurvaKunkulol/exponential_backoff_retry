4955c8979f384c8b56dc486ffcc51636128eeca1
Apurva Kunkulol <kunkulol.apurva@gmail.com>
2021-05-01 19:06:27 +0530

    Exponential backoff retrier.

---
+++ retrier/constants.py
@@ -0,0 +1,9 @@
+successful_poll_url = "https://api.openweathermap.org/data/2.5/weather?"
+unsuccessful_poll_url = "https://api.openweathermap.org/data/2.5/weathe"
+querystring = {
+    "q":"pune",
+    "appid":"f93f25cc41c7d9ceb83f1dd201ae0be4",
+}
+
+max_attempts = 5
+random_const = 2
